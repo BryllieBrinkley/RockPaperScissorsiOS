56701d819e00d0e1c96aa6a5049559aa1ea5e0c3
JIbryll Brinkley <49615544+BryllieBrinkley@users.noreply.github.com>
2024-05-26 13:16:38 -0400

    build controller, update UI, handle player button taps, app complete

--- RPS/GameState.swift
+++ RPS/GameState.swift
@@ -5,21 +5,42 @@
 //  Created by Jibryll Brinkley on 5/24/24.
 //
 
+
+
+// colors: .start - blue
+//.win = green
+// .lose == red
+// .draw = yellow
+
 import Foundation
+import UIKit
 
 enum GameState {
     case start, win, lose, draw
     
-    var message: String {
+    var status: String {
         switch self {
         case .start:
             return "Rock, Paper, or Scissors?"
         case .win:
-            return "You Win!"
+            return "You Won!"
         case .lose:
-            return "You Lose :("
+            return "You Lose!"
         case .draw:
-            return "Draw"
+            return "It's a Draw"
+        }
+    }
+    
+    var color: UIColor {
+        switch self {
+        case .start:
+            return UIColor.blue
+        case .win:
+            return UIColor.green
+        case .lose:
+            return UIColor.red
+        case .draw:
+            return UIColor.yellow
         }
     }
 }
--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -18,15 +18,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var playAgainButton: UIButton!
     
     @IBAction func rockChosen(_ sender: Any) {
+        play(userSign: .rock)
     }
     
     @IBAction func paperChosen(_ sender: Any) {
+        play(userSign: .paper)
     }
     
     @IBAction func scissorsChosen(_ sender: Any) {
+        play(userSign: .scissors)
     }
     
     @IBAction func playAgain(_ sender: Any) {
+        updateUI(state: .start)
     }
     
     
@@ -36,7 +40,65 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI(state: .start)
     }
     
+    func updateUI(state: GameState) {
+        statusLabel.text = state.status
+        switch state {
+            
+        case .start:
+            view.backgroundColor = .gray
+            signLabel.text = "🤖"
+            playAgainButton.isHidden = true
+            
+            rockButton.isEnabled = true
+            paperButton.isEnabled = true
+            scissorsButton.isEnabled = true
+            
+            rockButton.isHidden = false
+            paperButton.isHidden = false
+            scissorsButton.isHidden = false
+            
+            
+            
+        case .win:
+            view.backgroundColor = state.color
+        case .lose:
+            view.backgroundColor = state.color
+        case .draw:
+            view.backgroundColor = state.color
+        }
+        
+    }
     
+    func play(userSign: Sign) {
+        
+        let computerSign = randomSign()
+        
+        let gameState = userSign.gameState(against: computerSign)
+        
+        updateUI(state: gameState)
+        
+        signLabel.text = computerSign.emoji
+        
+        rockButton.isEnabled = false
+        paperButton.isEnabled = false
+        scissorsButton.isEnabled = false
+        
+        rockButton.isHidden = true
+        paperButton.isHidden = true
+        scissorsButton.isHidden = true
+        
+        playAgainButton.isHidden = false
+
+        switch userSign {
+        case .rock:
+            rockButton.isHidden = false
+        case .paper:
+            paperButton.isHidden = false
+        case .scissors:
+            scissorsButton.isHidden = false
+        }
+    }
 }
